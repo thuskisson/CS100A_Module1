e598772a4cbf8e436e4204bfa4f63045eb7d043c
thuskisson <thuskisson@pga.com>
2023-10-10 22:09:32 -0700

    Conditionals Checkpoint

---
+++ Module01_conditionals.py
@@ -0,0 +1,11 @@
+# Ask for their name
+name = input("Hello, what is your name? ")
+
+#Ask for their age, make sure it is of type int
+age = int(input(f"Hello {name}, what is your age? "))
+if age == 0:
+    print("That is not a valid age. ")
+if age == 16:
+    print("Congratulations, you can drive! ")
+if age >= 18:
+    print("You're eligible to vote. ")
