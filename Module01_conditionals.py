85513e62cf0ad5297cffb3d36283ad052844d118
thuskisson <thuskisson@pga.com>
2023-10-10 22:11:44 -0700

    added a condition for an input of 17

--- Module01_conditionals.py
+++ Module01_conditionals.py
@@ -7,5 +7,7 @@ if age == 0:
     print("That is not a valid age. ")
 if age == 16:
     print("Congratulations, you can drive! ")
+if age == 17:
+    print("You're almost eligible to vote. At least you can drive! ")
 if age >= 18:
     print("You're eligible to vote. ")
